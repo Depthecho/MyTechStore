0cfb2b24d1089780878817d9dea7014020447240
Depthecho <51miha7@gmail.com>
2023-09-08 16:32:09 +0300

    Theme swap has added

--- Store/mainpage/static/js/script.js
+++ Store/mainpage/static/js/script.js
@@ -1,12 +1,18 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function () {
-        $("#id_password1").on("focus", function () {
-            $("#password-hint").css("display", "block");
-        });
+function darkmode(){
+    console.log('Dark mode')
+    const body = document.body
+    const wasDarkmode = localStorage.getItem("darkmode") == "true"
 
-        $("#id_password1").on("blur", function () {
-            $("#password-hint").css("display", "none");
-        });
-    });
-</script>
+    localStorage.setItem("darkmode", !wasDarkmode)
+    body.classList.toggle('dark-mode', wasDarkmode)
+    document.getElementsByClassName("swap-theme")[0].children[0].classList.toggle('fa-sun', wasDarkmode)
+    document.getElementsByClassName("swap-theme")[0].children[0].classList.toggle('fa-moon', !wasDarkmode)
+}
+
+document.querySelector('.swap-theme').addEventListener('click', darkmode)
+
+
+function onload(){
+    document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') == 'true')
+}
+document.addEventListener('DOMContentLoaded', onload)
